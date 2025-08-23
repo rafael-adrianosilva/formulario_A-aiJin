dc112832a122e927ed7ee3e207aa1c3f7ae6e66c
Rafael Adriano <rafael.a.silva64@aluno.senai.br>
2025-08-23 19:36:00 -0300

    Página pedidos

---
+++ _js/main.js
@@ -0,0 +1,63 @@
+let P = 10;
+let M = 15;
+let G = 20;
+
+
+function fazerPedido() {
+    let pedido = prompt("Qual tamanho você deseja? (P, M, G)").toUpperCase();
+    switch (pedido) {
+        case "P":
+            alert("Você pediu uma Açaí Pequeno (250ml) por R$10,00");
+            P;
+            break;
+        case "M":
+            alert("Você pediu uma Açaí Médio (450ml) por R$15,00");
+            M;
+            break;
+        case "G":
+            alert("Você pediu uma Açaí Grande (750ml) por R$20,00");
+            G;
+            break;
+        default:
+            alert("Tamanho inválido!");
+    }
+
+    function formaPagamento() {
+
+        document.write("<p>PEDIDO:</p>");
+        if (pedido === P) {
+            document.write("Açaí: Pequeno (P), R$" + P + "<br>");
+        } else if (pedido === M) {
+            document.write("Açaí: Médio (M), R$" + M + "<br>");
+        } else {
+            document.write("Açaí: Grande (G), R$" + G + "<br>");
+            let forma = prompt("Qual a forma de pagamento? (Dinheiro, Cartão (acréscimo de 5%)").toLowerCase();
+            switch (forma) {
+                case "dinheiro":
+                    alert("Você escolheu pagar em Dinheiro.");
+                    break;
+                case "cartão":
+                    alert("Você escolheu pagar com Cartão (acréscimo de 5%).");
+                    if (pedido === P) {
+                        let valorC = P * 1.05;
+                        document.write("TOTAL COM ACRÉSCIMO: R$" + valorC + "<br>");
+                    } else if (pedido === M) {
+                        let valorC = M * 1.05;
+                        document.write("TOTAL COM ACRÉSCIMO: R$" + valorC + "<br>");
+                    } else {
+                        let valorC = G * 1.05;
+                        document.write("TOTAL COM ACRÉSCIMO: R$" + valorC + "<br>");
+                    }
+                    break;
+                default:
+                    alert("Forma de pagamento inválida!");
+            }
+        }
+    }
+    document.getElementById('formaPagamento').addEventListener('click', function () {
+        formaPagamento();
+    })
+}
+document.getElementById("fazerpedido").addEventListener("click", function () {
+    fazerPedido();
+});
